df6d977e3274440d2961c289d5186cb98285292f
Camila Liston <84218260+CAMILALISTON@users.noreply.github.com>
2026-05-31 13:28:29 -0300

    ajsutes12

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -17,6 +17,18 @@ app.use(cors({
     credentials: true
 }));
 app.options('*', cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
