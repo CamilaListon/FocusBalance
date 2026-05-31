b4fab24650353d5474b575418e257625039a0425
Camila Liston <84218260+CAMILALISTON@users.noreply.github.com>
2026-05-31 13:05:18 -0300

    ajsutes7

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,12 +10,13 @@ const dashboardRoutes = require('./routes/dashboardRoutes'); // Faremos em segui
 
 const app = express();
 
-// Middlewares globais
 app.use(cors({
-    origin: '*', // Permite que qualquer front-end acesse
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
